5170dbea91c514a3a8a59ec50fabdbb44a6843de
kang <winpro@ovencode.net>
2020-09-19 13:57:08 +0900

    Home

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -8,6 +8,7 @@ namespace gittest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hungry!");
+            Console.WriteLine("Go Home");
         }
     }
 }
